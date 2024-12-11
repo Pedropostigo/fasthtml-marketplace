bf6b683f1df681fc045e07b2e9174ad06c704dba
Pedro Postigo <postigo.laguna@gmail.com>
2024-12-11 12:45:47 +0100

    changed to use datetime python type for dates and times

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fasthtml.common import FastHTML, serve, RedirectResponse
 
 from app_src.events.create_event import CreateEventForm, Event
@@ -15,7 +17,11 @@ def create_event(event:Event):
 
     if CreateEventForm._validate_event(event):
         db = DataBase()
-        db.insert_event(event_name = event.event_name, event_date_time = event.event_date_time)
+        date, time = event.event_date_time.split("T")
+        year, month, day = date.split("-")
+        hour, minute = time.split(":")
+        db.insert_event(event_name = event.event_name, 
+                        event_date_time = datetime(int(year), int(month), int(day), int(hour), int(minute), 0))
 
     return CreateEventForm().render_success_creating_event(event)
 
--- app_src/events/event.py
+++ app_src/events/event.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 
+from datetime import datetime
+
 @dataclass
 class Event:
     event_name: str
-    event_date_time: str
+    event_date_time: datetime
